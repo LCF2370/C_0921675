dd81fb381c6d43eabd9c7345b08cbf88f147c7af
LCF2370 <c0921675@mylambton.ca>
2024-04-05 20:04:08 -0400

    version_14.1.6.2
    - styles.css added hiding the viewImage-container it will only displayed once the user select a category from select box
    - working unpslash API
    - Used a carousel from bootstrap for results of images from Unsplash API
    - Testing the layout from GitHub source

--- scripts/search-unsplash.js
+++ scripts/search-unsplash.js
@@ -1,26 +1,40 @@
 $(document).ready(function() {
   // Function to fetch images from Unsplash API
-  function fetchImages(query) {
-      const accessKey = 'S0rQa8LyyPQBT-Q0KXHxLHYE1T_nHUbJMsgMoiphF4w';
-      const apiUrl = `https://api.unsplash.com/search/photos?query=${query}&client_id=${accessKey}`;
+    function fetchImages(query) {
+        const accessKey = 'S0rQa8LyyPQBT-Q0KXHxLHYE1T_nHUbJMsgMoiphF4w';
+        const apiUrl = `https://api.unsplash.com/search/photos?query=${query}&client_id=${accessKey}`;
 
-      // Make a GET request to the Unsplash API
-      $.get(apiUrl, function(response) {
-          // Clear the image container
-          $('#searched-image').empty();
-
-          // Loop through the response and display images
-          response.results.forEach(function(photo) {
-              const imageUrl = photo.urls.regular;
-              const imageElement = `<img src="${imageUrl}" alt="Unsplash Image">`;
-              $('#viewImage').append(imageElement);
-          });
-      });
+        // Make a GET request to the Unsplash API
+        $.get(apiUrl, function(response) {
+            // Clear the image container
+            $('#viewImage').empty();
+            var count = 0;
+            // Loop through the response and display images
+            response.results.forEach(function(photo) {
+                count += 1;
+                const imageUrl = photo.urls.small;
+                if (count == 1){
+                    const imageElement = `<div class="carousel-item active"><img src="${imageUrl}" alt="Unsplash Image" class="rounded mx-auto d-block"></div>`;
+                    $('#viewImage').append(imageElement);
+                }
+                else{
+                    const imageElement = `<div class="carousel-item"><img src="${imageUrl}" alt="Unsplash Image" class="rounded mx-auto d-block"></div>`;
+                    $('#viewImage').append(imageElement);
+                }
+            });
+        });
   }
 
   // Event listener for the search button
-  $('#search-image').click(function() {
-      const searchQuery = $('#image-unsplash').val();
-      fetchImages(searchQuery);
+  $('#image-unsplash').click(function() {
+    const searchImage = $('#image-unsplash').val();
+    if(searchImage != '' && searchImage != 'Select Category'){
+        document.getElementById("viewImage-container").style.display = "block";
+        fetchImages(searchImage);
+    }
+    else{
+        $("#error").html('Field cannot be empty')
+        document.getElementById("viewImage-container").style.display = "none";
+    }
   });
 });
